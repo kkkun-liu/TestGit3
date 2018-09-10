7fbb43ac0e57659d2488707b398acdf3111ff1ab
kkkun-liu <1156589352@qq.com>
2018-09-10 10:28:43 +0800

    测试提交新类

---
+++ app/src/main/java/com/sun_tunnel/testgit3/Hello2.java
@@ -0,0 +1,8 @@
+package com.sun_tunnel.testgit3;
+
+public class Hello2 {
+    //AS中Create Git Repository与Github并无直接联系，只是再自己电脑本地创建一个仓库而已，
+    //接下来还是要再github上老老实实创建项目再从AS上根除选择提交把URL配成刚刚新建的github上新建仓库的URL
+
+    //相比之下，选择第二个就快多了，一步到位
+}
